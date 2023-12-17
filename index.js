eb591119d61c9265dd58ab8ad3f4f136c312e981
Mary Watiri <watirimary30@gmail.com>
2023-12-17 19:21:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+//Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+function destructivelyAppendCat(){
+cats.push("Ralph")
+return (cats);
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+    return (cats);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Ralph")
+    return(cats);
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Bob")
+    return(cats);
+}
+function appendCat(Rose){
+    return([...cats, Rose]);
+
+}
+function prependCat(Nelly){
+    return([Nelly ,...cats]);
+}
+function removeLastCat(){
+    var numberOfCats = cats.slice(0, cats.length-1);
+    return (numberOfCats);
+}
+function removeFirstCat(){
+     var latestCats = cats.slice(1);
+     return(latestCats);
+}
+ 
+
+
+
+    
